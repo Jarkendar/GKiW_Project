a15e4936a1be1a4bc33260e3719b18c35485c3e9
Items0 <nieznany012@wp.pl>
2017-05-17 20:48:13 +0200

    Ściany i podłoga

--- main_file.cpp
+++ main_file.cpp
@@ -37,23 +37,26 @@ void error_callback(int error, const char* description) {
 }
 
 float speed=3.14;
-
+float z_camera_position = -3;
+float x_camera_position = 5;
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-    if (action == GLFW_PRESS) {
-        if (key == GLFW_KEY_RIGHT) speed = 3.14/4;
-        if (key == GLFW_KEY_LEFT) speed =  - 3.14/4;
+    if (action == GLFW_REPEAT) {
+        if (key == GLFW_KEY_RIGHT)x_camera_position -= 0.3;
+        if (key == GLFW_KEY_LEFT) x_camera_position += 0.3;
+        if (key == GLFW_KEY_UP) z_camera_position += 0.3;
+        if (key == GLFW_KEY_DOWN) z_camera_position -= 0.3;
         }
-if (action == GLFW_RELEASE)
+/*if (action == GLFW_RELEASE)
 {
     if (key == GLFW_KEY_RIGHT || key == GLFW_KEY_LEFT) speed = 0;
-}
+}*/
 }
 //Procedura inicjująca
 void initOpenGLProgram(GLFWwindow* window) {
 	//************Tutaj umieszczaj kod, który należy wykonać raz, na początku programu************
    // glClearColor(0.12,0.4,0,1);
      glClearColor(0,1,1,1); // kolor tla
-    //glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_COLOR_MATERIAL);
@@ -68,50 +71,73 @@ void drawScene(GLFWwindow* window, float angle) {
 	mat4 M = mat4(1.0f);
 
 	mat4 V = lookAt(
-    vec3(0.0f,0.0f,-5.0f),
-    vec3(0.0f,0.0f,0.0f),
-    vec3(0.0f,1.0f,0.0f));
+    vec3(x_camera_position, 1.9f, z_camera_position),
+    vec3(10, 1.4f, -5),
+    vec3(0.0f, 1.0f, 0.0f)); // polozenie kamery
 
-    mat4 P = perspective(50*PI/180,1.0f,1.0f, 50.0f);
+    mat4 P = perspective(50*PI/180, 1.0f, 1.0f, 50.0f);
     glColor3d(1, 0.5, 0.1); // kolor rysowania
     glMatrixMode(GL_PROJECTION);
     glLoadMatrixf(glm::value_ptr(P));
     glMatrixMode(GL_MODELVIEW);
-    M = translate(M, vec3(1.0f,0.0f,0.0f));
-    M = rotate(M,angle,vec3(0.0f,0.0f,1.0f) );
-    glLoadMatrixf(glm::value_ptr(V*M));
+    //M = translate(M, vec3(1.0f,0.0f,1.0f));
+    //M = rotate(M,angle,vec3(0.0f,0.0f,1.0f) );
+    //glLoadMatrixf(glm::value_ptr(V*M));
 
-    Models::torus.drawSolid();
-
-    M = mat4(1.0f);
-    M = translate(M, vec3(-1.0f,0.0f,0.0f));
-    M = rotate(M,-angle,vec3(0.0f,0.0f,1.0f) );
-    glLoadMatrixf(glm::value_ptr(V*M));
-    Models::torus.drawSolid();
-
-
-
+    //Models::torus.drawSolid();
+    //glRectf(0,0,4,4);
+    //glColor3d(0, 0.5, 0.1);
+    //M = mat4(1.0f);
+    //M = translate(M, vec3(1.0f,0.0f,-1.0f));
+    //M = rotate(M,-angle,vec3(0.0f,0.0f,1.0f) );
+    //glLoadMatrixf(glm::value_ptr(V*M));
+    //Models::torus.drawSolid();
 
     M = mat4(1.0f);
     M = translate(M, vec3(0.0f,0.0f,0.0f));
-    //M = rotate(M,-angle,vec3(0.0f,0.0f,1.0f) );
+    M = rotate(M,-angle,vec3(0.0f,0.0f,1.0f) );
     glLoadMatrixf(glm::value_ptr(V*M));
-    float geomVertices[]={
-        0,0,0, 1,1,-1,
-        0,0,0, 1,-1,-1,
-        0,0,0, -1,-1,-1,
-        1,-1,-1, -1,-1,-1,
-        1,-1,-1, 1,-1,-1,
-    //-2.5,0,-1.44, 2.5,0,-1.44
-};
-int geomVertexCount=10;
+    float geomVerticesWalls[]={
+        0,0,0,
+        0,3,0,
+        16,3,0,
+        16,0,0, // sciana przod
 
-glColor3d(0, 0, 0); // kolor rysowania
+        16,0,0,
+        16,3,0,
+        16,3,-10,
+        16,0,-10, // sciana prawa
+
+        16,0,-10,
+        16,3,-10,
+        0,3,-10,
+        0,0,-10, // sciana tyl
+
+        0,0,-10,
+        0,3,-10,
+        0,3,0,
+        0,0,0, // sciana lewa
+};
+
+float geomVerticesFloor[]={
+        0,0,0,
+        16,0,0,
+        16,0,-10,
+        0,0,-10 // podloga
+};
+int geomVertexWallsCount = 16;
+int geomVertexFloorCount = 4;
+glColor3d(1, 0.5, 0.3); // kolor rysowania
 glEnableClientState(GL_VERTEX_ARRAY);
-glVertexPointer( 3, GL_FLOAT, 0, geomVertices);
-glDrawArrays(GL_LINES,0,geomVertexCount);
+glVertexPointer( 3, GL_FLOAT, 0, geomVerticesWalls);
+glDrawArrays(GL_QUADS,0,geomVertexWallsCount);
 glDisableClientState(GL_VERTEX_ARRAY);
 
+glColor3d(0.5, 0.1, 0.3);
+glEnableClientState(GL_VERTEX_ARRAY);
+glVertexPointer( 3, GL_FLOAT, 0, geomVerticesFloor);
+glDrawArrays(GL_QUADS,0,geomVertexFloorCount);
+glDisableClientState(GL_VERTEX_ARRAY);
 
 
     glfwSwapBuffers(window);
@@ -153,7 +179,7 @@ int main(void)
 	//Główna pętla
 	while (!glfwWindowShouldClose(window)) //Tak długo jak okno nie powinno zostać zamknięte
 	{
-	    angle+=speed*glfwGetTime();
+	    //angle+=speed*glfwGetTime();
         glfwSetTime(0);
 		drawScene(window, angle); //Wykonaj procedurę rysującą
 		glfwPollEvents(); //Wykonaj procedury callback w zalezności od zdarzeń jakie zaszły.
